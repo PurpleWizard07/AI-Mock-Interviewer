1b704b5a4910a05ae4b7a908e268e7b7df3c9d8f
PurpleWizard07 <purplewizard0709@gmail.com>
2025-07-11 00:31:16 +0530

    Initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import { Toaster } from "@/components/ui/sonner";
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Vader",
+  description: "An awesome AI-powered platform to prepare for mock interviews.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={`${monaSans.className} antialiased pattern`}>
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
